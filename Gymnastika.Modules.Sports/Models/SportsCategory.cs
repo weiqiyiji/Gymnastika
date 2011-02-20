86f694b1faca72a01cfa217586275c8be7b1dff3
unknown <Ruby@.(none)>
2011-02-20 19:20:25 +0800

    cc

--- Gymnastika.Modules.Sports/Models/SportsCategory.cs
+++ Gymnastika.Modules.Sports/Models/SportsCategory.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
 using Gymnastika.Modules.Sports.Services;
-using System.ComponentModel.Composition;
 
 namespace Gymnastika.Modules.Sports.Models
 {
