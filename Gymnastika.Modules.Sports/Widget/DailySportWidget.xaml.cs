c324b878150d0beab4b9e57e1a81e3aed3b4bfff
unknown <Ruby@.(none)>
2011-03-27 12:06:21 +0800

    s

--- Gymnastika.Modules.Sports/Widget/DailySportWidget.xaml.cs
+++ Gymnastika.Modules.Sports/Widget/DailySportWidget.xaml.cs
@@ -22,7 +22,7 @@ namespace Gymnastika.Modules.Sports.Widget
     /// <summary>
     /// Interaction logic for DailySportWidget.xaml
     /// </summary>
-    [WidgetMetadata("每日运动计划", "/Gymnastika.Modules.Sports;component/Resources/Images/Sport.jpg")]
+    [WidgetMetadata("每日运动计划", "/Gymnastika.Modules.Sports;component/Resources/Images/Sport.png")]
     public partial class DailySportWidget : UserControl , IWidget
     {
         DailySportViewModel _model;
