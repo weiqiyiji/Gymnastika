46a60500997b36c384a58017316bacf26be6f8dc
weiqiyiji <weiqiyiji@gmail.com>
2011-03-29 11:05:52 +0800

    Signed-off-by: weiqiyiji <weiqiyiji@gmail.com>

---
+++ Gymnastika.Phone/Sync/ScheduleItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gymnastika.Sync.Communication
+{
+    public class ScheduleItem
+    {
+        public int UserId { get; set; }
+        public int ConnectionId { get; set; }
+        public DateTime StartTime { get; set; }
+        public string Message { get; set; }
+    }
+}
